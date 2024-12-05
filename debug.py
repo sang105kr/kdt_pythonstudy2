f2b6da559445ec120619f48fab6ae1c5d05f2fd8
sanghak <sang105kr@gmail.com>
2024-12-05 17:38:25 +0900

    20241205-8

---
+++ debug.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+여러줄 주석
+Created on Wed Dec  4 09:33:38 2024
+
+@author: kh502
+"""
+
+# 한줄주석
+
+name = '홍길동'
+
+def sum(v1,v2) :
+  sum = v1 + v2
+  print(sum)
+  return sum
+
+
+sum(10,20)
